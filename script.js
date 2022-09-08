962cc38daeddb2978fd0203754b2e61964b7a60b
Lucas <lucass.ambar@gmail.com>
2022-09-08 18:24:34 -0300

    API pedido

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //escopo global
 let contador = 0;
+let usuario = prompt("Qual o seu nome?")
+let url, model, neck, material;
 
 function selecionaModelo (item) {
     let modelos = document.querySelectorAll(".modelo")
@@ -13,6 +15,17 @@ function selecionaModelo (item) {
     let img = item.querySelector(".img")
     img.classList.add("selecionado")
     contador += 1
+
+    if  (img.classList.contains("tshirt")) {
+        model = "t-shirt"
+    }
+    if  (img.classList.contains("camiseta")) {
+        model = "top-tank"
+    }
+    if  (img.classList.contains("manga")) {
+        model = "long"
+    }
+
     pedidoPronto()
 }
 
@@ -29,6 +42,17 @@ function selecionaGola(item) {
     let img = item.querySelector(".img")
     img.classList.add("selecionado")
     contador += 1
+
+    if  (img.classList.contains("v")) {
+        neck = "t-v-neck"
+    }
+    if  (img.classList.contains("redonda")) {
+        neck = "round"
+    }
+    if  (img.classList.contains("polo")) {
+        neck = "polo"
+    }
+
     pedidoPronto()
 }
 
@@ -45,17 +69,62 @@ function selecionaTecido(item) {
     let img = item.querySelector(".img")
     img.classList.add("selecionado")
     contador += 1
+
+    if  (img.classList.contains("seda")) {
+        material = "silk"
+    }
+    if  (img.classList.contains("algodao")) {
+        material = "cotton"
+    }
+    if  (img.classList.contains("poli")) {
+        material = "polyester"
+    }
+
+    pedidoPronto()
+}
+
+function link(input) {
+    console.log ("oi")
+    url = input.value;
+    let pattern = /^https:\/\//i;
+
+    if (!pattern.test(url)) {
+        alert("URL da imagem inválido!")
+        return
+    }
+
+    contador += 1
     pedidoPronto()
 }
 
 function pedidoPronto () {
     console.log(contador)
-    let input = document.querySelector("input").value
-    if (contador !== 3) {
+    if (contador !== 4) {
         return
     }
 
     let botao = document.querySelector(".button")
     botao.classList.remove("naopedir")
     botao.classList.add("pedir")
+}
+
+function pedir(botao) {
+    if(botao.classList.contains("naoPedir")) {
+        return
+    }
+    
+    let objeto = {
+        "model": model,
+        "neck": neck,
+        "material": material,
+        "image": url,
+        "owner": usuario,
+        "author": usuario
+    }
+    
+    let promessa = axios.post("https://mock-api.driven.com.br/api/v4/shirts-api/shirts", objeto)
+    promessa.then(() => {alert("Encomenda enviada com sucesso!")})
+    promessa.catch((erro) => {
+        console.log(erro)
+    })
 }
